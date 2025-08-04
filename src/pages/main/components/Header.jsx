42f805f42751b6fe5edb5b37114b97bf417461e4
ericmaeda <ericmaeda41@gmail.com>
2025-08-04 12:12:05 +0700

    Finished Projects.jsx as the project page (Not responsive)

--- src/pages/main/components/Header.jsx
+++ src/pages/main/components/Header.jsx
@@ -6,27 +6,31 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
     return (
         <>
-        <div class='lg:flex lg:justify-between md:flex md:flex-col md:mt-0 mt-24'>
-            <div className='md:flex md:flex-col xl:max-w-2xl lg:max-w-md md:max-w-3/5 max-w-4/5 xl:mx-30 lg:mx-20 xl:my-20 lg:my-7 md:mx-40 mt-10 ml-10 xl:p-10 lg:p-10 md:p-12 p-10
+        <div class='lg:flex lg:justify-between md:flex md:flex-col md:mt-0 mx-auto'>
+            <div className='md:flex md:flex-col xl:max-w-2xl lg:max-w-md md:max-w-3/5 max-w-4/5 xl:mx-35 lg:mx-20 xl:my-12 lg:my-auto md:mx-auto mx-auto mt-10 xl:p-10 lg:p-10 md:p-12 p-10
              text-white backdrop-blur border-2 rounded-3xl'>
                 <h1 className='md:text-2xl'>Good Morning!</h1>
                 <br />
                 <h1 className='md:text-6xl text-3xl'>I'm Erico Mahendra</h1>
                 <br />
                 <p>A highly motivated software engineer. Fueled by curiosity, driven by clean architecture, and obsessed with making things that scale. Currently exploring the art of backend systems, cloud infrastructure, and everything in between.</p>
+                <br />
+                <div className='w-full'>
+                    <a href="" className='inline-block text-white rounded-full p-3 px-6 hover:text-blue-900 transition duration-300' style={{ backgroundImage: 'linear-gradient(to right, #f00, #00f)' }}>Download CV</a>
+                </div>
             </div>
-            <div className='lg:flex lg:flex-col lg:absolute xl:top-3/15 lg:top-3/13 xl:right-1/9 lg:right-1/9 lg:mx-0 lg:mt-5 lg:my-0 md:mx-56 mx-10 md:my-12 mt-12 xl:p-10 lg:p-6 md:max-w-1/2 max-w-2xs border-2 text-white backdrop-blur rounded-3xl'>
+            <div className='lg:flex lg:flex-col lg:absolute xl:top-3/15 lg:top-3/13 xl:right-1/9 lg:right-1/9 lg:mx-0 lg:mt-5 lg:my-0 md:mx-auto mx-auto md:my-12 mt-12 xl:p-10 lg:p-6 md:max-w-1/2 max-w-2xs border-2 text-white backdrop-blur rounded-3xl'>
                 <div className='lg:flex lg:justify-center md:flex md:justify-center lg:p-0 md:p-10'>
                     <img src="/eric.png" alt="eric-pic" className='lg:w-70 lg:h-70 md:w-60 md:h-60 w-45 h-45 md:mx-0 mx-13 md:my-0 my-7 rounded-full'/>
                 </div>
                 <div className='flex justify-center text-5xl py-4'>
-                    <a href="https://github.com/ericmaeda" target="_blank" className=''>
+                    <a href="https://github.com/ericmaeda" target="_blank" className='cursor-pointer hover:text-gray-300 transition duration-200'>
                         <FontAwesomeIcon icon={ faGithub }/>
                     </a>
-                    <a href="https://linkedin.com/in/ericmaeda27" target="_blank" className=''>
+                    <a href="https://linkedin.com/in/ericmaeda27" target="_blank" className='cursor-pointer hover:text-gray-300 transition duration-200'>
                         <FontAwesomeIcon icon={ faLinkedin }/>
                     </a>
-                    <a href="mailto:ericopbm@gmail.com" className=''>
+                    <a href="mailto:ericopbm@gmail.com" className='cursor-pointer hover:text-gray-300 transition duration-200'>
                         <FontAwesomeIcon icon={ faEnvelope }/>
                     </a>
                 </div>
--- src/pages/main/components/Projects.jsx
+++ src/pages/main/components/Projects.jsx
@@ -1,11 +1,64 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact, faNodeJs } from '@fortawesome/free-brands-svg-icons'
 
 const Projects = () => {
     return (
         <>
-        <div id='Projects' className='flex justify-center text-white'>
-            <div className='px-8 py-20 mt-5'>
-                <h1 className='text-4xl border-2 rounded-full p-5 px-2'>My Projects</h1>
+        <div id='Projects' className='flex flex-col w-full text-white'>
+            <div className='w-full px-8 py-20 mt-5 m-auto'>
+                <div className='flex justify-center'>
+                    <div className='inline-block text-3xl border-2 backdrop-blur rounded-full p-5 px-10'>
+                    My Projects
+                    </div>
+                </div>
+            </div>
+            <div id='project-boxes' className='grid grid-cols-3 gap-5 mx-auto'>
+                <div className='border-2 rounded-3xl backdrop-blur'>
+                    <div className='p-10'>
+                        <img src="/eric-site.png" alt="" className='mx-auto w-80 border-1 rounded-2xl'/>
+                    </div>
+                    <div className='flex flex-col text-center max-w-md'>
+                        <div className='text-2xl'>Portfolio Website</div>
+                        <div className='p-5'>My portfolio website. Contains my profile, projects, education, etc. Crafted with MERN stacks, decorated with TailwindCSS.</div>
+                        <div className='flex flex-col'>
+                            <div className='flex justify-center border-1 rounded-xl gap-5 items-center p-3 mx-auto'>
+                                <img src="/mongo-db.png" alt="" className='w-5 h-15'/>
+                                <img src="/expressjs-icon.svg" alt="" className='bg-white rounded-full p-2 w-12'/>
+                                <img src="/react-2.svg" alt="" className='w-12'/>
+                                <img src="/nodejsLight.svg" alt="" className='w-30'/>
+                                <img src="/tailwindcss.svg" alt="" className='w-12'/>
+                            </div>
+                            <br />
+                            <div className='flex justify-center gap-24 items-center pb-6 mx-auto'>
+                                <a href="" target='_blank' className='border-1 rounded-full p-3'>Demo</a>
+                                <a href="" target='_blank' className='border-1 rounded-full p-3'>GitHub Repo</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='border-2 rounded-3xl backdrop-blur'>
+                    <div className='p-10'>
+                        <img src="/bem-chatting.png" alt="" className='mx-auto w-80 border-1 rounded-2xl'/>
+                    </div>
+                    <div className='flex flex-col text-center max-w-md'>
+                        <div className='text-2xl'>BEM Chatting</div>
+                        <div className='p-5'>A web based chatting platform to fulfill the recruiment task for Business and Partnership Bureau of BEM Fasilkom. Crafted with MERN stacks, my sleeping hours, and decorated with TailwindCSS.</div>
+                        <div className='flex flex-col'>
+                            <div className='flex justify-center border-1 rounded-xl gap-5 items-center p-3 mx-auto'>
+                                <img src="/mongo-db.png" alt="" className='w-5 h-15'/>
+                                <img src="/expressjs-icon.svg" alt="" className='bg-white rounded-full p-2 w-12'/>
+                                <img src="/react-2.svg" alt="" className='w-12'/>
+                                <img src="/nodejsLight.svg" alt="" className='w-30'/>
+                                <img src="/tailwindcss.svg" alt="" className='w-12'/>
+                            </div>
+                            <br />
+                            <div className='flex justify-center gap-24 items-center pb-6 mx-auto text-gray-300'>
+                                <div className='border-1 rounded-full p-3'>Soon</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         </>
